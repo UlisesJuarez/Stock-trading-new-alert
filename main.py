ce13b2121a53529a6da69b1e371cb8bc6e3104ee
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-03-04 22:18:17 -0600

    send the sms messages

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from dotenv import load_dotenv
 import os
+from twilio.rest import Client
 
 load_dotenv()
 
@@ -8,6 +9,9 @@ ALPHA_KEY=os.getenv("ALPHA_KEY")
 NEWS_KEY=os.getenv("NEWS_KEY")
 STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
+account_sid = os.getenv('TWILIO_ACCOUNT_SID')
+auth_token = os.getenv('TWILIO_AUTH_TOKEN')
+my_number=os.getenv("PHONE")
 
 STOCK_ENDPOINT = "https://www.alphavantage.co/query"
 NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
@@ -34,12 +38,16 @@ print(yesterday_closing_price)
 print(day_before_yesterday_closing_price)
 
 price_difference=abs(float(yesterday_closing_price)-float(day_before_yesterday_closing_price))
-print(price_difference)
+up_down=None
+if price_difference>0:
+    up_down="😎"
+else:
+    up_down="😥"
 
-diff_percent=(price_difference/float(yesterday_closing_price))*100
+diff_percent=round((price_difference/float(yesterday_closing_price))*100)
 print(diff_percent)
 
-if(diff_percent>0.1):
+if abs(diff_percent)>=0:
     news_params={
         "apiKey":NEWS_KEY,
         "qInTitle":COMPANY_NAME,
@@ -48,14 +56,14 @@ if(diff_percent>0.1):
     articles=news_res.json()["articles"]
     three_articles=articles[:3]
     print(three_articles)
-#Optional TODO: Format the message like this: 
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
-
+    
+    formatted_articles=[f"{STOCK_NAME}: {up_down}{diff_percent}%\nHeadline:{article['title']}. \nBrief:{article['description']}" for article in three_articles]
+    client = Client(account_sid, auth_token)
+    
+    for article in formatted_articles:
+        message = client.messages \
+            .create(
+                body=article,
+                from_='+17622165590',
+                to=my_number
+            )
